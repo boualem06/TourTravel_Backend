1c623c9f50cb03a21f090ed05a6d854b2fe36bb8
boualem06 <kb_hamroune@esi.dz>
2023-03-01 23:28:40 +0100

    finishing the backend of Tour

--- Controllers/tourController.js
+++ Controllers/tourController.js
@@ -2,6 +2,11 @@ const Tour=require('../models/tourModel')
 
 
 const NewTour=async(req,res)=>{
+    // if(req.user.admin===false){
+    //     res.status(400) ;
+    //     res.json( {message:"Only admins can add New Tours ",status:400})
+    //     return ;
+    // }
     const {title,city,address,distance,photo,desc,price,maxGroupSize,featured}=req.body  ;
 
     if(!title || !city || !address || !distance || !photo || !desc || !price || !maxGroupSize || (featured!=false && featured!=true))
@@ -36,10 +41,48 @@ const NewTour=async(req,res)=>{
         maxGroupSize:tour.maxGroupSize ,
         featured:tour.featured
     })
+}
 
+
+const getTours=async (req,res)=>{
+    const tours=await Tour.find() ;
+    res.status(200).json({
+      "toursArray":tours
+    })
+}
+
+const getTourById=async (req,res)=>{
+    const tour=await Tour.findById(req.params.tourId) ;
+    res.status(200).json(
+        tour
+    )
+}
+
+const deleteTour=async(req,res)=>{
+    console.log(req.body.id)
+    const deletedTour= await Tour.deleteOne({_id:req.body.id})
+    console.log(deletedTour)
+    res.status(200).json(
+        deletedTour
+    )
+}
+
+const searchTour=async(req,res)=>{
+    const tours=await Tour.find({
+        "address":req.params.address,
+        "distance":req.params.distance,
+        "maxGroupSize" : {$gte:req.params.maxGroupSize}
+    })
+    res.status(200).json(
+        tours
+    )
 }
 
 
 module.exports={
     NewTour,
+    getTours,
+    getTourById,
+    deleteTour,
+    searchTour
 }
--- routes/TourRoutes.js
+++ routes/TourRoutes.js
@@ -4,4 +4,9 @@ const tourControllers=require('../Controllers/tourController')
 const {protect}=require('../midleware/authmidleware')
 
 router.post("/NewTour",protect,tourControllers.NewTour) ;
+router.get("/getTours",protect,tourControllers.getTours) ;
+router.get("/getTours/:tourId",protect,tourControllers.getTourById) ;
+router.delete("/deleteTourById",protect,tourControllers.deleteTour) ;
+router.get("/searchTour/:address/:distance/:maxGroupSize",protect,tourControllers.searchTour) ;
+
 module.exports=router ;
